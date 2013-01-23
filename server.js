bbfd2a5f1841525583e09370112fcf8b590c3786
purplefox <timvolpe@gmail.com>
2013-01-23 14:33:30 +0000

    delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-load('vertx.js')
-
-var port = parseInt(vertx.env['OPENSHIFT_INTERNAL_PORT'])
-var host = vertx.env['OPENSHIFT_INTERNAL_IP']
-
-vertx.createHttpServer().requestHandler(function(req) {
-  req.response.end("<html><body><h1>Hello from vert.x!</h1></body></html>");
-}).listen(port, host);
